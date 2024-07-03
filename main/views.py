d152e04a557c2679ffb0da3829013a1c8cf32d1f
BLACK F4X <vertalet910@gmail.com>
2024-07-03 14:45:41 +0500

    Делаем магазин дальше

--- main/views.py
+++ main/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     context: dict[str, str] = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - HOME',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'bool': 'True'
+        'title': 'M&B - Главная',
+        'content': "Магазин"
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context: dict[str, str] = {
+        'title': 'M&B - О нас',
+        'content': "О нас",
+        'text_on_page': "ЕБАТЬ ОКНО АХУЕННОЕ"
+    }
+
+    return render(request, 'main/about.html', context)
