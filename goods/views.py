bbe51a02551f5d4161ca404d453314c1b2907686
BLACK F4X <vertalet910@gmail.com>
2024-08-17 21:07:24 +0500

    Slug converter for categories

--- goods/views.py
+++ goods/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from goods.models import Product
 
-def catalog(request):
+def catalog(request, category_slug):
 
-    goods = Product.objects.all()
+    if category_slug == 'all':
+        goods = Product.objects.all()
+    else:
+        goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
     context = {
         "title": "M&B - Каталог",
