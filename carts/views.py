51544a11d6890a1311641cc57f200d596601aba9
BLACK F4X <vertalet910@gmail.com>
2024-08-21 00:00:59 +0500

    ajax + корзина = дичь

--- carts/views.py
+++ carts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 from carts.models import Cart
+from carts.utils import get_user_carts
 from goods.models import Product
 
-def cart_add(request, product_slug):
-    product = Product.objects.get(slug=product_slug)
+def cart_add(request):
+
+    product_id = request.POST.get("product_id")
+    product = Product.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         try:
@@ -13,7 +18,16 @@ def cart_add(request, product_slug):
         except Cart.DoesNotExist:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META['HTTP_REFERER'])
+    user_cart = get_user_carts(request)
+
+    cart_items_html: Any = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html,
+    }
+    return JsonResponse(response_data)
 
 def cart_change(request, product_slug):
     ...
