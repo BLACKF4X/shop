5d55b8b65f31800ec55448b983a1bc3bee3a043d
BLACK F4X <vertalet910@gmail.com>
2024-08-23 02:41:55 +0500

    новые фиксы

--- goods/views.py
+++ goods/views.py
@@ -1,22 +1,25 @@
-from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.http import Http404
+from django.shortcuts import render
+
 from goods.models import Product
 from goods.utils import q_search
 
 
 def catalog(request, category_slug=None):
-
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
     query = request.GET.get('q', None)
 
-    if category_slug == 'all':
+    if category_slug == "all":
         goods = Product.objects.all()
     elif query:
         goods = q_search(query)
     else:
-        goods = Product.objects.filter(category__slug=category_slug)  # Changed this line
+        goods = Product.objects.filter(category__slug=category_slug)
+        if not goods.exists():
+            raise Http404()
 
     if on_sale:
         goods = goods.filter(discount__gt=0)
@@ -28,19 +31,16 @@ def catalog(request, category_slug=None):
     current_page = paginator.page(int(page))
 
     context = {
-        "title": "M&B - Каталог",
+        "title": "Home - Каталог",
         "goods": current_page,
         "slug_url": category_slug
     }
-    return render(request, 'goods/catalog.html', context)
+    return render(request, "goods/catalog.html", context)
 
 
 def product(request, product_slug):
+    product = Product.objects.get(slug=product_slug)
 
-    product = get_object_or_404(Product, slug=product_slug)
+    context = {"product": product}
 
-    context = {
-        'product': product
-    }
-
-    return render(request, 'goods/product.html', context=context)
+    return render(request, "goods/product.html", context)
