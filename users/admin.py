b62300c7c6edf16bd77a10a4cfaf67817613039b
BLACK F4X <vertalet910@gmail.com>
2024-08-23 00:03:20 +0500

    Заказики

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from users.models import User
 from carts.admin import CartTabAdmin
+from orders.admin import OrderTabulareAdmin
+
+from users.models import User
 
 
 @admin.register(User)
@@ -8,4 +10,4 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "first_name", "last_name", "email", ]
     search_fields = ["username", "first_name", "last_name", "email", ]
 
-    inlines = [CartTabAdmin, ]
+    inlines = [CartTabAdmin, OrderTabulareAdmin]
